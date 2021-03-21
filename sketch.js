bada93649e1f21f4e8d0d02b72b2baf8e176a0ca
Krishna0622 <66967827+Krishna0622@users.noreply.github.com>
2021-03-20 20:35:47 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,21 +9,23 @@ function preload()
 	qdog = loadImage('images/dogImg.png')
   happyDog = loadImage('images/dogImg1.png')
 }
-
+function readStock(data){
+  foodS=data.val();
+}
 function setup() {
-	createCanvas(500, 500);
-  dog = createSprite(50,50,250,250)
+	createCanvas(500,500);
+  dog = createSprite(250,250,100,100)
   dog.addImage(qdog);
-  database= firebaseConfig
+  dog.scale=0.3
+  database = firebase.database();
   foodStock=database.ref('Food')
   foodStock.on("value",readStock)
-  
 }
 
 
 function draw() {  
 background(46,139,87)
-if(KeyWentDown(UP_ARROW)){
+if(keyWentDown(UP_ARROW)){
   writeStock(foodS)
   dog.addImage(happyDog)
 }
@@ -31,9 +33,7 @@ if(KeyWentDown(UP_ARROW)){
   textSize(20)
   fill('white')
   stroke(1)
-  function readStock(data){
-    foodS=data.val();
-  }
+ 
   function writeStock(x){
     database.ref('/').update({
       Food:x
